0d35985d45af48c2c0df729ba05e25f43bde0272
dayo <dahurdayo@example.com>
2023-02-10 13:28:39 +0100

    add To cart

--- README.md
+++ README.md
@@ -22,3 +22,10 @@
     4. show product info in second column
     5. add styles
     
+4. Handle Add to Cart
+    1. define react context
+    2. define cart items state
+    3. create add to cart action
+    4. add to reducer
+    5. create store provider 
+    6. handle add to cart button
--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useContext } from 'react';
+import { Store } from '../utilis/Store';
 
 function Layout({title, children}) {
+  const {state} = useContext(Store)
+  const {cart} = state;
+ 
   return (
     <>
        <Head>
@@ -19,7 +24,16 @@ function Layout({title, children}) {
 
               <div>
                 <Link href="/cart" legacyBehavior>
-                  <a className='p-2'>cart</a>
+                  <a className='p-2'>
+                    cart
+                  {cart.cartItems.length > 0 && (
+                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
+                      {cart.cartItems.reduce((a, c)=>a+c.quantity, 0)}
+ 
+                    </span>
+
+                  )}
+                  </a>
                   </Link>
                 <Link href="/login" legacyBehavior>
                   <a className='p-2'>login</a>
